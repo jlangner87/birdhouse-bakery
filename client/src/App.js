497f642655e1270f983773a5d81216915a6df7d8
jlangner87 <josh.langner@gmail.com>
2023-05-24 10:58:54 -0500

    initial react application and dir structure

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './styles/App.css'
+import './styles/Mobile.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Birdhouse Bakery</h1>
+    </div>
+  )
+}
+
+export default App
