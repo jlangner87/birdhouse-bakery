c13f6f191c07b9ccba904d91388a6886fa48f58a
jlangner87 <josh.langner@gmail.com>
2023-08-01 10:58:29 -0500

    add farmer's market content

--- client/src/components/about.jsx
+++ client/src/components/about.jsx
@@ -12,7 +12,8 @@ function About() {
       <p>After moving to Spencer with her husband Jim in 2021, Chef Ron decided to go for her dream of owning her own bakery. Since then, Birdhouse Bakery has become a beloved destination for those seeking the perfect treats for their special occasions.</p>
       <p>As a pastry chef, Robin’s expertise in dessert design produces delightful treats that are a beautiful sight to see and a delicious bite to eat. Her creativity and self-expression come together to create a balance of flavors that are sure to satisfy your sweet tooth.</p>
       <p>Contact Birdhouse Bakery in Spencer, IA, to discover the perfect blend of artistry, taste, and passion. Let Robin Kruse create a stunning dessert for your next event, ensuring an unforgettable experience for you and your guests. Trust in their expertise to make your sweetest dreams come true.</p>
-    
+      <h3>Farmer's Market</h3>
+      <p>In addition to offering custom orders for special events, you can pick up some of Chef Robin's goodies all summer long at the Lakes Area Farmers Market. Check out their <a href="https://bestfarmersmarkets.org/place/lakes-area-farmers-market-spirit-lake-ia.html">current schedule</a>, and plan a time to indulge your tastebuds.</p>
       <img src={decor1} id="headshot"/>
     </div>
   )
